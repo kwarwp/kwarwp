044ca7f3fd8ea127b097b758bdbdabda50beb037
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 20:29:05 -0300

    Saved kwarwp/kwarapp.py @2020-07-28 23:29

--- kwarwp/kwarapp.py
+++ kwarwp/kwarapp.py
@@ -20,7 +20,10 @@ class Kwarwp():
     OCA = "https://i.imgur.com/dZQ8liT.jpg"
     INDIO = "https://imgur.com/8jMuupz.png"
     SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://i.imgur.com/JFgVVBH.jpg"
+    TORA = "https://imgur.com/ldI7IbK.png"
+    PICHE = "https://imgur.com/tLLVjfN.png"
+    CEU = "https://i.imgur.com/UAETaiP.gif"
+    SOL = "https://i.imgur.com/PfodQmT.gif"
     
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
@@ -30,8 +33,11 @@ class Kwarwp():
         """ Cria o ambiente de programação Kwarwp."""
         cena = self.v.c(self.SOLO)
         indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, cena=cena)
+        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
         tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
+        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
+        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
+        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
         cena.vai()
         return cena
         
