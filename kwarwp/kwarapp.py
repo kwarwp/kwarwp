98ab2fea9bdfca584cb9136cc3a696bf97d3590c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 19:55:42 -0300

    Saved kwarwp/kwarapp.py @2020-07-28 22:55

--- kwarwp/kwarapp.py
+++ kwarwp/kwarapp.py
@@ -17,7 +17,10 @@ class Kwarwp():
     
         :param vitollino: Empacota o engenho de jogo Vitollino.
     """
-    ABERTURA = "https://i.imgur.com/dZQ8liT.jpg"
+    OCA = "https://i.imgur.com/dZQ8liT.jpg"
+    INDIO = "https://imgur.com/8jMuupz.png"
+    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
+    TORA = "https://i.imgur.com/JFgVVBH.jpg"
     
     def __init__(self, vitollino=None, cenario="default"):
         self.v = vitollino()
@@ -25,7 +28,10 @@ class Kwarwp():
         
     def cria(self, cenario="default"):
         """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.ABERTURA)
+        cena = self.v.c(self.SOLO)
+        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
+        oca = self.v.a(self.OCA, w=100, h=100, x=500, cena=cena)
+        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
         cena.vai()
         return cena
         
@@ -35,5 +41,6 @@ def main(vitollino):
         
     
 if __name__ == "__main__":
-    from _spy.vitollino.main import Jogo
+    from _spy.vitollino.main import Jogo, STYLE
+    STYLE.update(width=600, height="500px")
     main(Jogo)
