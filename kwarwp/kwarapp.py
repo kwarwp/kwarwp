62cc5c0c3d04689ea19e0d0ba1fe00039d060c50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-30 11:33:32 -0300

    Saved kwarwp/kwarapp.py @2020-07-30 14:33

--- kwarwp/kwarapp.py
+++ kwarwp/kwarapp.py
@@ -5,25 +5,24 @@
 .. codeauthor:: Carlo Oliveira <carlo@ufrj.br>
 
 Classes neste módulo:
-    :py:class:`Taba` Arena onde os desafios ocorrem.
     :py:class:`Kwarwp` Jogo para ensino de programação.
 
 Changelog
 ---------
 .. versionadded::    20.07
-        classe Taba.
+        Usa um mapa de caracteres para colocar os elementos.
         classe Kwarwp.
 
 """
 MAPA_INICIO = """
-@...&
-.....
-.....
-.#.^.
+@....&
+......
+......
+.#.^..
 """
 
 
-class Taba():
+class Kwarwp():
     """ Arena onde os desafios ocorrem.
     
         :param vitollino: Empacota o engenho de jogo Vitollino.
@@ -32,7 +31,8 @@ class Taba():
     GLIFOS = {
     "&": "https://i.imgur.com/dZQ8liT.jpg",  # OCA
     "^": "https://imgur.com/8jMuupz.png",  # INDIO
-    ".": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO
+    ".": "https://i.imgur.com/npb9Oej.png",  # VAZIO
+    "_": "https://i.imgur.com/sGoKfvs.jpg",  # SOLO
     "#": "https://imgur.com/ldI7IbK.png",  # TORA
     "@": "https://imgur.com/tLLVjfN.png",  # PICHE
     "~": "https://i.imgur.com/UAETaiP.gif",  # CEU
@@ -41,47 +41,25 @@ class Taba():
     
     def __init__(self, vitollino=None, mapa=MAPA_INICIO, medidas={}):
         self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
+        self.cena = self.cria(mapa=mapa) if vitollino else None
         
-    def cria(self, cenario="default"):
-        """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
-        cena.vai()
-        return cena
+    def cria(self, mapa="  "):
+        """ Cria o ambiente de programação Kwarwp.
 
-
-class Kwarwp():
-    """ Jogo para ensino de programação.
-    
-        :param vitollino: Empacota o engenho de jogo Vitollino.
-    """
-    OCA = "https://i.imgur.com/dZQ8liT.jpg"
-    INDIO = "https://imgur.com/8jMuupz.png"
-    SOLO = "https://i.imgur.com/sGoKfvs.jpg"
-    TORA = "https://imgur.com/ldI7IbK.png"
-    PICHE = "https://imgur.com/tLLVjfN.png"
-    CEU = "https://i.imgur.com/UAETaiP.gif"
-    SOL = "https://i.imgur.com/PfodQmT.gif"
-    
-    def __init__(self, vitollino=None, cenario="default"):
-        self.v = vitollino()
-        self.cena = self.cria(cenario=cenario) if vitollino else None
-        
-    def cria(self, cenario="default"):
-        """ Cria o ambiente de programação Kwarwp."""
-        cena = self.v.c(self.SOLO)
-        indio = self.v.a(self.INDIO, w=100, h=100, x=300, y=400, cena=cena)
-        oca = self.v.a(self.OCA, w=100, h=100, x=500, y=100, cena=cena)
-        tora = self.v.a(self.TORA, w=100, h=100, x=100, y=400, cena=cena)
-        piche = self.v.a(self.PICHE, w=100, h=100, x=100, y=100, cena=cena)
-        piche = self.v.a(self.CEU, w=600, h=100, x=0, y=0, cena=cena)
-        sol = self.v.a(self.SOL, w=60, h=60, x=0, y=40, cena=cena)
+            :param mapa: Um texto representando o mapa do desafio.
+        """
+        """Cria um matriz com os elementos excluindo a primeira e última linha"""
+        mapa = mapa.split()[1:-1]
+        print(mapa)
+        """Largura da casa da arena dos desafios, número de colunas no mapa"""
+        lado, col = 100, len(mapa[0]) 
+        """Cria um cenário com imagem de terra de chão batido, céu e sol"""
+        cena = self.v.c(self.GLIFOS["_"])
+        ceu = self.v.a(self.GLIFOS["~"], w=lado*col, h=lado, x=0, y=0, cena=cena)
+        sol = self.v.a(self.GLIFOS["*"], w=60, h=60, x=0, y=40, cena=cena)
+        """Posiciona os elementos segundo suas posições i, j na matriz mapa"""
+        [self.v.a(self.GLIFOS[imagem], w=lado, h=lado, x=i*lado, y=j*lado, cena=cena)
+            for j, linha in enumerate(mapa) for i, imagem in enumerate(linha)]
         cena.vai()
         return cena
         
