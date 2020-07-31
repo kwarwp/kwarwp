d8643f23d5609df90d303293342ff006ba9252a4
carlotolla <carlotolla@gmail.com>
2020-07-31 12:47:00 -0300

    Draft for Taba

--- kwarwp/kwarapp.py
+++ kwarwp/kwarapp.py
@@ -6,6 +6,7 @@
 
 Classes neste módulo:
     :py:class:`Kwarwp` Jogo para ensino de programação.
+    :py:class:`Taba` Arena onde os desafios ocorrem.
 
 Changelog
 ---------
@@ -14,6 +15,8 @@ Changelog
         classe Kwarwp.
 
 """
+from collections import namedtuple as nt
+
 MAPA_INICIO = """
 @....&
 ......
@@ -22,9 +25,43 @@ MAPA_INICIO = """
 """
 
 
-class Kwarwp():
+class Taba():
     """ Arena onde os desafios ocorrem.
     
+        :param vitollino: Empacota o engenho de jogo Vitollino.
+        :param mapa: Um texto representando o mapa do desafio.
+
+    Na taba cada casa se relaciona com quatro outras, ao 
+    **Norte, Sul, Leste e Oeste**
+
+    .. mermaid::
+
+        graph TD
+            Casa --> Oeste & Norte & Leste & Sul 
+    
+    """
+def __init__(self, cena, taba=None, mapa=MAPA_INICIO):
+    self.v = Kwarwp.VITOLLINO
+    self.taba = taba if taba else self.cria(mapa)
+    self.norte = self.sul = self.leste = self.oeste = self
+
+def cria(self, mapa):
+    Fab = nt("Fab", "action image")
+    fabrica = {
+    "&": Fab(self.coisa, "https://i.imgur.com/dZQ8liT.jpg"), # OCA
+    "^": Fab(self.indio, "https://imgur.com/8jMuupz.png"),   # INDIO
+    ".": Fab(self.vazio, "https://i.imgur.com/npb9Oej.png"), # VAZIO
+    "_": Fab(self.coisa, "https://i.imgur.com/sGoKfvs.jpg"), # SOLO
+    "#": Fab(self.coisa, "https://imgur.com/ldI7IbK.png"),   # TORA
+    "@": Fab(self.coisa, "https://imgur.com/tLLVjfN.png"),   # PICHE
+    "~": Fab(self.coisa, "https://i.imgur.com/UAETaiP.gif"), # CEU
+    "*": Fab(self.coisa, "https://i.imgur.com/PfodQmT.gif"), # SOL
+    "|": Fab(self.coisa, " https://i.imgur.com/uwYPNlz.png")  # CERCA       
+    }
+
+class Kwarwp():
+    """ Jogo para ensino de programação.
+
         :param vitollino: Empacota o engenho de jogo Vitollino.
         :param mapa: Um texto representando o mapa do desafio.
     """
