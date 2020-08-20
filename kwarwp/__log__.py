6e662c2874ea38abdc1a335d13e326310750e9b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-19 21:09:01 -0300

    Saved kwarwp/__log__.py @2020-08-20 00:09

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -133,3 +133,29 @@ AttributeError: 'Jogo' object has no attribute 'e'
     self.posicao = (x//lado,y//lado-1)
 TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
 '''},
+{'date': 'Wed Aug 19 2020 21:09:00.567 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 396
+    main(Jogo, STYLE)
+  module <module> line 390
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 236
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 270
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 338
+    coisa = Piche(imagem, x=0, y=0, cena=cena, taba=self)
+  module kwarwp.kwarwpart line 152
+    self.posicao = (x//lado,y//lado-1)
+TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
+'''},
