843b3c32379cf4387a86bf5df063bd701b1804f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-19 21:20:57 -0300

    Saved kwarwp/__log__.py @2020-08-20 00:20

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -211,3 +211,29 @@ AttributeError: 'NoneType' object has no attribute 'a'
     self.vazio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=0, y=0, cena=cena)
 AttributeError: 'Jogo' object has no attribute 'a'
 '''},
+{'date': 'Wed Aug 19 2020 21:20:56.285 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 396
+    main(Jogo, STYLE)
+  module <module> line 390
+    Kwarwp(vitollino, medidas=medidas)
+  module <module> line 236
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 270
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 339
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module kwarwp.kwarwpart line 52
+    self.posicao = (x//lado,y//lado-1)
+TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
+'''},
