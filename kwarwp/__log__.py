34ac3269c6a6e6be74ddb899b58348f7a97d41df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-21 08:37:15 -0300

    Saved kwarwp/__log__.py @2020-08-21 11:37

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -237,3 +237,27 @@ AttributeError: 'Jogo' object has no attribute 'a'
     self.posicao = (x//lado,y//lado-1)
 TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
 '''},
+{'date': 'Fri Aug 21 2020 08:37:14.916 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 396
+    main(Jogo, STYLE)
+  module <module> line 390
+    Kwarwp(vitollino, medidas=medidas).cria()
+  module <module> line 270
+    self.taba = {(i, j): fabrica[imagem].objeto(fabrica[imagem].imagem, x=i*lado, y=j*lado+lado, cena=cena)
+  module <module> line 339
+    vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
+  module kwarwp.kwarwpart line 54
+    self.posicao = (x//lado,y//lado-1)
+TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
+'''},
