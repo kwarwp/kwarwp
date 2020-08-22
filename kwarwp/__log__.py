b05e7470909a18e0aecd48d4b100658d39d8a3a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-22 09:25:31 -0300

    Saved kwarwp/__log__.py @2020-08-22 12:25

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -329,3 +329,23 @@ TypeError: unsupported operand type(s) for //: 'int' and 'NoneType'
     self.indio = Kwarwp.VITOLLINO.a(imagem, w=lado, h=lado, x=x, y=y, cena=cena)
 AttributeError: 'NoneType' object has no attribute 'a'
 '''},
+{'date': 'Sat Aug 22 2020 09:25:30.675 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 400
+    main(Jogo, STYLE)
+  module <module> line 394
+    Kwarwp(vitollino, medidas=medidas).cria()
+  module <module> line 249
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'indi0'
+'''},
