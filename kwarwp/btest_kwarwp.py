375bce6038a001fd769e3db98ed4a5e199411e3e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-10 17:31:03 -0300

    Saved kwarwp/btest_kwarwp.py @2020-09-10 17:31

--- kwarwp/btest_kwarwp.py
+++ kwarwp/btest_kwarwp.py
@@ -101,7 +101,7 @@ class Test_Kwarwp(TestCase):
         self.assertEqual(100, coisa.lado, f"but coisa.lado was {coisa.lado}")
         tora = self.elts[self.TORA]
         self.assertEqual(coisa.ocupante.vazio, tora, f"but coisa.ocupante.indio was {coisa.ocupante.vazio}")
-        # self.assertEqual((0, 0), tora.pos, f"but tora.pos was {tora.pos}")
+        self.assertEqual((0, 0), tora.pos, f"but tora.pos was {tora.pos}")
         
     def _not(self):
         self.assertEqual((0, 0), tora.pos, f"but tora.pos was {tora.pos}")
