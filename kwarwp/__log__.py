00e0d1288f74b29d6522a12083641c66772dc375
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-14 13:43:28 -0300

    Saved kwarwp/__log__.py @2020-09-14 16:43

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -526,3 +526,5 @@ TypeError: Object passed as base class is not a class
 'error': ''''''},
 {'date': 'Mon Sep 14 2020 13:42:26.399 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
 'error': ''''''},
+{'date': 'Mon Sep 14 2020 13:43:27.687 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': ''''''},
