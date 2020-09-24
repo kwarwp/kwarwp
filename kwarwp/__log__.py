567db54edbee99d91e5d106880a85de0f1105662
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-24 13:19:35 -0300

    Saved kwarwp/__log__.py @2020-09-24 16:19

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -695,3 +695,19 @@ Traceback (most recent call last):
     vaga = Vazio("", x=x, y=y, cena=cena, ocupante=coisa)
 TypeError: __init__ missing 1 positional argument: 'taba'
 '''},
+{'date': 'Thu Sep 24 2020 13:19:34.649 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    main()
+TypeError: main() missing 2 positional arguments: vitollino,medidas
+'''},
