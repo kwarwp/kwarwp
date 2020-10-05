f444190d1c39bc059fedfb5168e69ecf9c5f2d9b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-05 18:12:56 -0300

    Saved kwarwp/__log__.py @2020-10-05 21:12

--- kwarwp/__log__.py
+++ kwarwp/__log__.py
@@ -833,3 +833,25 @@ AttributeError: 'NoneType' object has no attribute 'fala'
     self.ceu.elt.html = texto
 AttributeError: 'Kwarwp' object has no attribute 'ceu'
 '''},
+{'date': 'Mon Oct 05 2020 18:12:55.719 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 608
+    main(Jogo, STYLE)
+  module <module> line 602
+    return Kwarwp(vitollino=vitollino_proxy, medidas=medidas, mapa=mapa, indios=indios)
+  module <module> line 423
+    self.cena = self.cria(mapa=self.mapa) if vitollino else None
+  module <module> line 433
+    fabrica = {
+AttributeError: 'Kwarwp' object has no attribute 'apedra'
+'''},
